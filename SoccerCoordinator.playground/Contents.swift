46e5abe88a6b482c37bfff5a202b11fecf307358
Eric Romanik <eric@id8.com>
2017-11-18 14:40:31 -0800

    Working project with extra credit height balancing

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -2,155 +2,137 @@
 
 // Create array of dictionaries for all players
 
-let players: [[String: Any]] = [
-
+var players: [[String: Any]] = [
     [
         "name": "Joe Smith",
         "heightInInches": 42,
         "soccerExperience": true,
         "guardianNames": "Jim and Jan Smith"
     ],
-
     [
         "name": "Jill Tanner",
         "heightInInches": 36,
         "soccerExperience": true,
         "guardianNames": "Clara Tanner"
     ],
-
     [
         "name": "Bill Bon",
         "heightInInches": 43,
         "soccerExperience": true,
         "guardianNames": "Sara and Jenny Bon"
     ],
-
     [
         "name": "Eva Gordon",
         "heightInInches": 45,
         "soccerExperience": false,
         "guardianNames": "Wendy and Mike Gordon"
     ],
-
     [
         "name": "Matt Gill",
         "heightInInches": 40,
         "soccerExperience": false,
         "guardianNames": "Charles and Sylvia Gill"
     ],
-
     [
         "name": "Kimmy Stein",
         "heightInInches": 41,
         "soccerExperience": false,
         "guardianNames": "Bill and Hillary Stein"
     ],
-
     [
         "name": "Sammy Adams",
         "heightInInches": 45,
         "soccerExperience": false,
         "guardianNames": "Jeff Adams"
     ],
-
     [
         "name": "Karl Saygan",
         "heightInInches": 42,
         "soccerExperience": true,
         "guardianNames": "Heather Bledsoe"
     ],
-
     [
         "name": "Suzane Greenberg",
         "heightInInches": 44,
         "soccerExperience": true,
         "guardianNames": "Henrietta Dumas"
     ],
-
     [
         "name": "Sal Dali",
         "heightInInches": 41,
         "soccerExperience": false,
         "guardianNames": "Gala Dali"
     ],
-
     [
         "name": "Joe Kavalier",
         "heightInInches": 39,
         "soccerExperience": false,
         "guardianNames": "Sam and Elaine Kavalier"
     ],
-
     [
         "name": "Ben Finkelstein",
         "heightInInches": 44,
         "soccerExperience": false,
         "guardianNames": "Aaron and Jill Finkelstein"
     ],
-
     [
         "name": "Diego Soto",
         "heightInInches": 41,
         "soccerExperience": true,
         "guardianNames": "Robin and Sarika Soto"
     ],
-
     [
         "name": "Chloe Alaska",
         "heightInInches": 47,
         "soccerExperience": false,
         "guardianNames": "David and Jamie Alaska"
     ],
-
     [
         "name": "Arnold Willis",
         "heightInInches": 43,
         "soccerExperience": false,
         "guardianNames": "Claire Willis"
     ],
-
     [
         "name": "Phillip Helm",
         "heightInInches": 44,
         "soccerExperience": true,
         "guardianNames": "Thomas Helm and Eva Jones"
     ],
-
     [
         "name": "Les Clay",
         "heightInInches": 42,
         "soccerExperience": true,
         "guardianNames": "Wynona Brown"
     ],
-
     [
         "name": "Herschel Krustofski",
         "heightInInches": 45,
         "soccerExperience": true,
         "guardianNames": "Hyman and Rachel Krustofski"
     ]
-
 ]
 
 // Reorder player array by height for extra credit
 
 var orderedPlayersByHeight: [[String: Any]] = []
-var minimumHeight: Int = 100
-var shortestPlayer: [String: Any]
-/*
+var minimumHeight: Int
+var shortestPlayer: [String: Any] = players[0]
+var shortestPlayerIndex: Int = 0
+
 while players.count > 0 {
-    for player in players {
+    minimumHeight = 100
+    for (index, player) in players.enumerated() {
         let playerHeight: Int = player["heightInInches"] as! Int
         if playerHeight <= minimumHeight {
             shortestPlayer = player
             minimumHeight = playerHeight
+            shortestPlayerIndex = index
         }
     }
+    orderedPlayersByHeight.append(shortestPlayer)
+    players.remove(at:shortestPlayerIndex)
 }
- 
-*/
-//let index = array.index(of:"beta") {
-//    array.remove(at: index)
 
 // Create empty arrays of players for each team
 
@@ -173,7 +155,7 @@ func experiencedPlayers(in players: [[String : Any]]) -> Int {
 
 // Iterate through players and append to team arrays
 
-for player in players {
+for player in orderedPlayersByHeight {
     if let experienced = player["soccerExperience"] as? Bool {
         if experienced == true {
             let dragonsExperienced = experiencedPlayers(in: dragons)
@@ -248,12 +230,15 @@ func averageHeight(team: [[String: Any]]) -> Double {
 }
 
 let averageDragonHeight = averageHeight(team: dragons)
+print("Average Height of Dragons:")
 print(averageDragonHeight)
 
 let averageSharkHeight = averageHeight(team: sharks)
+print("Average Height of Sharks:")
 print(averageSharkHeight)
 
 let averageRaptorHeight = averageHeight(team: raptors)
+print("Average Height of Raptors:")
 print(averageRaptorHeight)
 
 
